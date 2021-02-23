12762e58773bf960f80f40bdc4712065bbf9434c
Rozanna <rozanna.boczar@gmail.com>
2021-02-23 23:23:49 +0100

    added more data to the model

--- WatashiNoAnimeList/AnimeList/models.py
+++ WatashiNoAnimeList/AnimeList/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import PIL
 
 # Create your models here. - model = table in the database
 
@@ -13,16 +13,27 @@ class Studio(models.Model):
 
 
 class Anime(models.Model):
+    # ability to change the score or create your own
     # null - default field empty
     # blank - can be empty, no value needed
     studio = models.ForeignKey(Studio, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=500, null=True)
+    name_eng = models.CharField(max_length=500, null=True)
     description = models.TextField(blank=True, null=True)
     num_episodes = models.IntegerField(blank=True, null=True)
+    status = models.CharField(choices=[("Currently Airing", "Currently Airing"), ("Finished Airing", "Finished Airing")]
+                              , max_length=256, null=True, blank=True)
+    premiered = models.CharField(blank=True, null=True, max_length=256) # add choices like Winter Year
+    source = models.CharField(blank=True, null=True, max_length=256) # add choices manga, novel, game, whatever
+    genres = models.CharField(blank=True, null=True, max_length=256) # also create genres a lot of genres
+    duration = models.DecimalField(blank=True, null=True, decimal_places=2, max_digits=256) # add
+    rating = models.CharField(blank=True, null=True, max_length=256) # or just type the age
     year = models.IntegerField(blank=True, null=True)
+    score = models.IntegerField(blank=True, null=True)
+    ranked = models.IntegerField(blank=True, null=True)
+    image = models.ImageField(verbose_name=None, blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return self.name_eng
 
     # to change plural names on the view
     # class Meta:
